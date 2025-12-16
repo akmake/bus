b6fe238be3a17d9fb4eaaae153a16dbb4fe236d6
akmake <92978132+akmake@users.noreply.github.com>
2025-12-16 15:09:43 +0200

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://yosefdaean_db_user:lPkTYXPmJ1TBJiyt@cluster0.3o2c7zy.mongodb.net/?appName=Cluster0');
+    // התיקון: הוספתי את "cityline_db" אחרי ה-.net/ ולפני ה-?
+    const uri = process.env.MONGO_URI || 'mongodb+srv://yosefdaean_db_user:lPkTYXPmJ1TBJiyt@cluster0.3o2c7zy.mongodb.net/cityline_db?appName=Cluster0';
+    
+    const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
